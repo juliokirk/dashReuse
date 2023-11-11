83ff7628063180d89cd9c866cd4c9622735eee8a
Julles <juliokirk@gmail.com>
2023-11-11 01:52:21 -0300

    Ajustes

--- js/mainChart.js
+++ js/mainChart.js
@@ -7,10 +7,10 @@ const myChart = new Chart(ctx, {
       label: '# of Tomatoes',
       data: [12, 19, 3, 5],
       backgroundColor: [
-        'rgba(255, 99, 132, 0.5)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)'
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)'
       ],
       borderColor: [
         'rgba(255,99,132,1)',
