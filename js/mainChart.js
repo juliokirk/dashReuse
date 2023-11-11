374943b731d78befe6fc42540eaea7d51c9ddebb
Julles <juliokirk@gmail.com>
2023-11-11 00:49:58 -0300

    Ajustes

--- js/mainChart.js
+++ js/mainChart.js
@@ -2,19 +2,24 @@ const ctx = document.getElementById('myChart');
 
   new Chart(ctx, {
       
-    type: 'pie',
+    type: 'doughnut',
     data = {
-    datasets: [{
-        data: [10, 20, 30]
-    }],
-
-    // These labels appear in the legend and in the tooltips when hovering different arcs
-    labels: [
+      labels: [
         'Red',
-        'Yellow',
-        'Blue'
-    ]
-};
+        'Blue',
+        'Yellow'
+      ],
+      datasets: [{
+        label: 'My First Dataset',
+        data: [300, 50, 100],
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)',
+          'rgb(255, 205, 86)'
+        ],
+        hoverOffset: 4
+      }]
+    };
       
     options: {
       scales: {
