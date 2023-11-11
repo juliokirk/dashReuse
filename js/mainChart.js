19b15d47482fb1c9c0e80d47ef7278eef35782ef
Julles <juliokirk@gmail.com>
2023-11-11 01:26:37 -0300

    Ajustes

--- js/mainChart.js
+++ js/mainChart.js
@@ -1,33 +1,29 @@
-import Chart from 'chart.js/auto'
-
-const ctx = document.getElementById('myChart');
-
-  new Chart(ctx, {
-      
-    type: 'doughnut',
-    data = {
-      labels: [
-        'Red',
-        'Blue',
-        'Yellow'
+const ctx = document.getElementById("myChart");
+const myChart = new Chart(ctx, {
+  type: 'pie',
+  data: {
+    labels: ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'],
+    datasets: [{
+      label: '# of Tomatoes',
+      data: [12, 19, 3, 5],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.5)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)'
       ],
-      datasets: [{
-        label: 'My First Dataset',
-        data: [300, 50, 100],
-        backgroundColor: [
-          'rgb(255, 99, 132)',
-          'rgb(54, 162, 235)',
-          'rgb(255, 205, 86)'
-        ],
-        hoverOffset: 4
-      }]
-    };
-      
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+   	//cutoutPercentage: 40,
+    responsive: false,
+
+  }
+});
