9a89d52255bd38fb50c0c2f7f6d65442e0237905
Julles <juliokirk@gmail.com>
2023-11-10 23:56:39 -0300

    Grafico certo adicionado

--- js/mainChart.js
+++ js/mainChart.js
@@ -1,15 +1,21 @@
 const ctx = document.getElementById('myChart');
 
   new Chart(ctx, {
+      
     type: 'pie',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
-    },
+    data = {
+    datasets: [{
+        data: [10, 20, 30]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+        'Red',
+        'Yellow',
+        'Blue'
+    ]
+};
+      
     options: {
       scales: {
         y: {
