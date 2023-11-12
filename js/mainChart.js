41081a3cc166b26aaf9b7c93063c48c8da79823e
Julles <juliokirk@gmail.com>
2023-11-12 12:01:37 -0300

    Ajustes gerais

--- js/mainChart.js
+++ js/mainChart.js
@@ -5,16 +5,16 @@ const myChart = new Chart(ctx, {
     labels: ['Plástico', 'Metal', 'Vidro', 'Orgânicos', 'Rejeitos'],
     datasets: [{
       label: 'Kg',
-      data: [1002.56, 4385.55, 1560.75, 433.2, 10.72],
+      data: [1002.56, 5688.73, 1727.03, 433.2, 10.72],
       backgroundColor: [
         'rgba(173, 38, 23, 1)',
-        'rgba(37, 23, 173, 1)',
-        'rgba(23, 173, 95, 1)',
         'rgba(173, 151, 23, 1)',
-        'rgba(88, 46, 41, 1)'
+        'rgba(23, 173, 95, 1)',
+        'rgba(88, 46, 41, 1)',
+        'rgba(98, 112, 102, 1)'
       ],
       borderColor: [
-         'rgba(173, 38, 23, 1)',
+        'rgba(173, 38, 23, 1)',
         'rgba(37, 23, 173, 1)',
         'rgba(23, 173, 95, 1)',
         'rgba(173, 151, 23, 1)',
