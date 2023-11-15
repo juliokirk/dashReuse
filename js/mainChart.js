02e795200e57893f6ce9803202392f6e1176c679
Julles <juliokirk@gmail.com>
2023-11-15 18:58:16 -0300

    Calculo de porcentagem e outros ajustes

--- js/mainChart.js
+++ js/mainChart.js
@@ -1,11 +1,31 @@
 const ctx = document.getElementById("myChart");
+
+let plas = 1002.56
+let metal = 5688.73
+let vidro = 1727.03
+let orgs = 433.2
+let rejs = 10.72
+
+let total = plas + metal + vidro + orgs + rejs
+
+function percentage(number, sum){
+  let perc = (number/sum)*100
+  return Math.round(perc * 10)/10
+}
+
+let div1 =  percentage(plas, total)
+let div2 =  percentage(metal, total)
+let div3 =  percentage(vidro, total)
+let div4 =  percentage(orgs, total)
+let div5 =  percentage(rejs, total)
+
 const myChart = new Chart(ctx, {
   type: 'pie',
   data: {
-    labels: ['Plástico', 'Metal', 'Vidro', 'Orgânicos', 'Rejeitos'],
+    labels: ['Plástico (' + plas + ' Kg)', 'Metal (' + metal + ' Kg)', 'Vidro (' + vidro + ' Kg)', 'Orgânicos (' + orgs + ' Kg)', 'Rejeitos (' + rejs + ' Kg)'],
     datasets: [{
-      label: 'Kg',
-      data: [1002.56, 5688.73, 1727.03, 433.2, 10.72],
+      label: 'Parcela do total (em %)',
+      data: [div1, div2, div3, div4, div5],
       backgroundColor: [
         'rgba(173, 38, 23, 1)',
         'rgba(173, 151, 23, 1)',
@@ -32,7 +52,9 @@ const myChart = new Chart(ctx, {
         labels: {
           font: {
             size: 20
-          }
+          },
+          position: 'bottom',
+          display: false
         }
       }
     }
